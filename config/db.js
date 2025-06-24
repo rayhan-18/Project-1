8b03ee90546ec89715918bf2a6c42701390fcd6d
rayhan-18 <mr0031689@gmail.com>
2025-06-24 20:49:32 +0700

    update

--- config/db.js
+++ config/db.js
@@ -4,10 +4,10 @@
   dotenv.config();
 
   const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'mysql-ncyw.railway.internal',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'tokorayhan',
+    database: process.env.DB_NAME || 'railway',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
